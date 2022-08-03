c7258d22a6a500178a3b9328ce75d8ff17307f76
Sergei Ampilov <sergei.ampilov@gmail.com>
2022-08-03 19:39:25 +0300

    refactor messages for user model

--- models/card.js
+++ models/card.js
@@ -3,25 +3,28 @@ const mongoose = require('mongoose');
 const cardSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, 'Name is required'],
+    minlength: [2, 'Too short. Must be at least 2, got {VALUE}'],
+    maxlength: [30, 'Too long. Must be max 30, got {VALUE}'],
   },
   link: {
     type: String,
-    required: true,
+    required: [true, 'Link is requred'],
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
-    required: true,
+    required: [true, 'Owner id is required'],
   },
   likes: [{
     type: mongoose.Schema.Types.ObjectId,
     ref: 'user',
     default: [],
   }],
-  createdAt: {},
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('card', cardSchema);
