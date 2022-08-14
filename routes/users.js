c104840c3ac19debb1c14ea7b91106855b0bc6ea
Sergei Ampilov <sergei.ampilov@gmail.com>
2022-08-14 20:35:13 +0300

    feat middleware for auth ready

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,11 @@
 const router = require('express').Router();
 
 const {
-  getUser, createUser, getAllUsers, updateUser, updateAvatar,
+  getUser, getAllUsers, updateUser, updateAvatar,
 } = require('../controllers/users');
 
 router.get('/:userId', getUser); /* возвращает пользователя по userId */
 router.get('/', getAllUsers); /* возвращает всех пользователей */
-router.post('/', createUser); /* создаёт пользователя */
 router.patch('/me', updateUser); /* обновляет профиль */
 router.patch('/me/avatar', updateAvatar); /* обновляет аватар */
 
