5b7239f303c446f8b87f8bd7a56d0fa17b1d53cf
Sergei Ampilov <sergei.ampilov@gmail.com>
2022-08-15 21:42:22 +0300

    fix token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,21 +1,27 @@
 const jwt = require('jsonwebtoken');
-const BadUserError = require('../errors/bad-user-error');
+// const BadUserError = require('../errors/bad-user-error');
 
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
+  const token = req.cookies.jwt;
 
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new BadUserError('Необходима авторизация'));
+  if (!token) {
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация.' });
   }
 
-  const token = authorization.replace('Bearer ', '');
   let payload;
-
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    return next(new BadUserError('Необходима авторизация'));
+    return res
+      .status(401)
+      .send({ message: 'Необходима авторизация.' });
+
+    // next(new BadUserError('Необходима авторизация..'));
+    // throw new BadUserError('Необходима авторизация.');
   }
   req.user = payload;
-  return next();
+  next();
+  return true;
 };
