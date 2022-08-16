169b2cb886eae1166f84e5872f52d5effc82d7be
Sergei Ampilov <sergei.ampilov@gmail.com>
2022-08-16 15:19:37 +0300

    refactor middleware for errors

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { login, createUser } = require('./controllers/users');
 const NotFoundError = require('./errors/not-found-error');
 const auth = require('./middlewares/auth');
 const { checkSignIn, checkSignUp } = require('./middlewares/celebrate');
+const { processError } = require('./middlewares/error');
 
 const { PORT = 3000 } = process.env;
 
@@ -24,27 +25,13 @@ app.post('/signup', checkSignUp, createUser);
 app.use(auth);
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
-app.use('*', (req, res, next) => {
-  try {
-    throw new NotFoundError('Страница не найдена');
-  } catch (err) {
-    next(err);
-  }
-});
+app.use('*', (req, res, next) => next(new NotFoundError('Страница не найдена')));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
 });
 
 app.use(errors()); // обработчик ошибок celebrate
-
-// здесь обрабатываем все ошибки
-app.use((err, req, res, next) => {
-  const { statusCode = 500, message } = err;
-  res.status(statusCode).send({
-    message: statusCode === 500 ? 'На сервере произошла ошибка' : `Произошла ошибка ${message}`,
-  });
-  return next;
-});
+app.use(processError);
 
 app.listen(PORT, () => {});
---
+++ middlewares/error.js
@@ -0,0 +1,7 @@
+module.exports.processError = (err, req, res, next) => {
+  const { statusCode = 500, message } = err;
+  res.status(statusCode).send({
+    message: statusCode === 500 ? 'На сервере произошла ошибка' : `Произошла ошибка ${message}`,
+  });
+  next();
+};
